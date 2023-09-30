f1b65f4dda7d04fd73c23a3d22b5cb7b0fba95ed
equaynor <>
2023-09-30 15:12:46 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log('Connected');
+function runGame () {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function increment Score() {
+
+}
+
+function incrementWronAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
